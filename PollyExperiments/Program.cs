8e4a12bf2087d1485953a774b256d8306ecc5867
Agatino Pesce <agatino.pesce@gmail.com>
2023-03-04 05:31:24 +0100

    Works with extension method

--- PollyExperiments/Program.cs
+++ PollyExperiments/Program.cs
@@ -16,19 +16,38 @@ internal class Program
     }
 }
 
+public static class PollyExtensions
+{
+
+
+    public static AsyncRetryPolicy<TResult> WaitAndRetrySelectiveAsyncPolicy<TResult>(Func<TResult, bool> handlePredicate, Func<DelegateResult<TResult>, TimeSpan> sleepDurationProvider)
+        => Policy.HandleResult(handlePredicate).WaitAndRetryForeverAsync<TResult>((_, result, _) => sleepDurationProvider(result), (_, _, _, _) => Task.CompletedTask);
+
+    //public static AsyncRetryPolicy<TResult> WaitAndRetryForeverAsync<TResult>(this PolicyBuilder<TResult> policyBuilder, Func<DelegateResult<TResult>, TimeSpan> sleepDurationProvider)
+    //    => policyBuilder.WaitAndRetryForeverAsync<TResult>((_, delegResult, _) => sleepDurationProvider(delegResult), (_, _, _, _) => Task.CompletedTask);
+
+    public static AsyncRetryPolicy<TResult> WaitAndRetryForeverAsync<TResult>(this PolicyBuilder<TResult> policyBuilder, Func<TResult, TimeSpan> sleepDurationProvider)
+    {
+        return policyBuilder.WaitAndRetryForeverAsync<TResult>((_, delegResult, _) => sleepDurationProvider(delegResult.Result), (_, _, _, _) => Task.CompletedTask);
+    }
+}
+
 public class LimitedPatience
 {
     public async Task GiveItATryAsync()
     {
-        AsyncTimeoutPolicy<string?> timeoutPolicy =
+        AsyncTimeoutPolicy<string?> timeout =
             Policy.TimeoutAsync<string?>(25);
 
-        AsyncRetryPolicy<string?> retryPolicy =
+        AsyncRetryPolicy<string?> retryNullOrBrokenPolicy =
             Policy.Handle<BrokenCircuitException>()
             .OrResult<string?>(s => s != "MyText")
             .RetryForeverAsync(onRetry: async (exception, retryCount, context) =>
             {
-                if (exception != null) { await Task.Delay(TimeSpan.FromSeconds(2)); }
+                if (exception != null)
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(2));
+                }
             });
 
         AsyncCircuitBreakerPolicy<string?> circuitBreakerPolicy =
@@ -36,25 +55,48 @@ public class LimitedPatience
             .CircuitBreakerAsync(2, TimeSpan.FromSeconds(4));
 
         //BrokenCircuitException
-        var wrapPolicy =
-            Policy.WrapAsync<string?>(timeoutPolicy, retryPolicy, circuitBreakerPolicy);
+        var wrapPolicy1 =
+            Policy.WrapAsync<string?>(timeout, retryNullOrBrokenPolicy, circuitBreakerPolicy);
+
+        ///////////////////////
+        
+        var retryOnNotMatching =
+            Policy.HandleResult<string?>(s => s != "MyText")
+            .RetryForeverAsync();
+
+        var waitAndRetryOnNull =
+            Policy.HandleResult<string?>(s => s == null)
+            .WaitAndRetryForeverAsync(counter => TimeSpan.FromSeconds(3));
+
+        var wrapPolicy2 =
+            Policy.WrapAsync<string?>(timeout, retryOnNotMatching, waitAndRetryOnNull);
+
+        ///////////////////////
+        
+        AsyncRetryPolicy<string?> waitAndRetryselective = Policy.HandleResult<string?>(s => s != "MyText")
+            .WaitAndRetryForeverAsync<string?>((_, s, _) => TimeSpan.FromSeconds(s == null ? 3 : 0), (_, _, _) => Task.CompletedTask);
+
+
+        AsyncRetryPolicy<string?> waitAndRetrySelectiveExtension =
+            Policy.HandleResult<string?>(s => s != "MyText")
+            .WaitAndRetryForeverAsync<string?>(s=> TimeSpan.FromSeconds(s == null ? 3 : 0));
+        var wrapPolicy3 = Policy.WrapAsync<string?>(timeout, waitAndRetrySelectiveExtension);
+
 
         try
         {
             string? found =
-                await wrapPolicy.ExecuteAsync(async (ct) => await InterruptableSingleAttemptAsync(ct), CancellationToken.None);
+                await wrapPolicy3.ExecuteAsync(async (ct) => await InterruptableSingleAttemptAsync(ct), CancellationToken.None);
             if (found != null) { WriteLine(found); }
-            
-
-
         }
         catch (TimeoutRejectedException)
         {
             WriteLine("Timeut Expired");
         }
         await Task.Delay(1000);
-
     }
+
+   
 
     public async Task<string?> InterruptableSingleAttemptAsync(CancellationToken cancellationToken)
     {
